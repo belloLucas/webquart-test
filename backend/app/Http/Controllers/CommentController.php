681ff5b98f63d21134f167e6d7ef6770859cff6f
belloLucas <lucasbellodev@gmail.com>
2024-02-25 16:17:52 -0300

    feat: Adding Comment controller

---
+++ backend/app/Http/Controllers/CommentController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use Illuminate\Routing\Controller;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    public function index()
+    {
+        $comments = Comment::all();
+        return response()->json($comments);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'user_id' => 'required|integer|exists:users,id',
+            'house_id' => 'required|integer|exists:houses,id',
+            'comment' => 'required|string',
+            'avaliation_note' => 'required|integer'
+        ]);
+
+        $comment = Comment::create($validatedData);
+        return response()->json($comment, 201);
+    }
+
+    public function show(Comment $comment)
+    {
+        return response()->json($comment);
+    }
+
+    public function update(Request $request, Comment $comment)
+    {
+        $validatedData = $request->validate([
+            'comment' => 'required|string',
+            'avaliation_note' => 'required|integer'
+        ]);
+
+        $comment->update($validatedData);
+        return response()->json($comment, 200);
+    }
+
+    public function delete(Comment $comment)
+    {
+        $comment->delete();
+        return response()->json(null, 204);
+    }
+
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\HouseController;
+use App\Http\Controllers\CommentController;
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -21,3 +22,9 @@ Route::post('/houses', [HouseController::class, 'store']);
 Route::get('/houses/{house}', [HouseController::class, 'show']);
 Route::put('/houses/{house}', [HouseController::class, 'update']);
 Route::delete('/houses/{house}', [HouseController::class, 'delete']);
+
+Route::get('/comments', [CommentController::class, 'index']);
+Route::post('/comments', [CommentController::class, 'store']);
+Route::get('/comments/{comment}', [CommentController::class, 'show']);
+Route::put('/comments/{comment}', [CommentController::class, 'update']);
+Route::delete('/comments/{comment}', [CommentController::class, 'delete']);
