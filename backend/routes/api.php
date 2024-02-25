f2a098482dcef73f172ba9c9b7aaa65b359ff560
belloLucas <lucasbellodev@gmail.com>
2024-02-25 14:18:45 -0300

    feat: Adding house controller to router

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\HouseController;
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -14,3 +15,9 @@ Route::post('/users', [UserController::class, 'store']);
 Route::get('/users/{user}', [UserController::class, 'show']);
 Route::put('/users/{user}', [UserController::class, 'update']);
 Route::delete('/users/{user}', [UserController::class, 'delete']);
+
+Route::get('/houses', [HouseController::class, 'index']);
+Route::post('/houses', [HouseController::class, 'store']);
+Route::get('/houses/{house}', [HouseController::class, 'show']);
+Route::put('/houses/{house}', [HouseController::class, 'update']);
+Route::delete('/houses/{house}', [HouseController::class, 'delete']);
