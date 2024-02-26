24246767e630ce9b1067c81c103d323792d3e8e8
belloLucas <lucasbellodev@gmail.com>
2024-02-26 09:39:51 -0300

    feat: HouseService implemented

--- backend/app/Http/Controllers/HouseController.php
+++ backend/app/Http/Controllers/HouseController.php
@@ -6,66 +6,39 @@ use App\Models\House;
 use App\Models\Comment;
 use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
+use App\Services\HouseService;
 
 class HouseController extends Controller
 {
+    public function __construct(HouseService $houseService)
+    {
+        $this->houseService = $houseService;
+    }
+
     public function index()
     {
-        $houses = House::all();
-        return response()->json($houses);
+        return $this->houseService->findAllHouses();
     }
 
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
-            'user_id' => 'required|integer|exists:users,id',
-            'house_title' => 'required|string',
-            'house_description' => 'required|string',
-            'rent_price' => 'required|integer',
-            'house_picture' => 'required|string',
-            'bedrooms' => 'required|integer',
-            'restrooms' => 'required|integer',
-            'bills_included' => 'required|boolean',
-            'street' => 'required|string',
-            'neighborhood' => 'required|string',
-            'city' => 'required|string',
-            'state' => 'required|string'
-        ]);
-
-        $house = House::create($validatedData);
-        return response()->json($house, 201);
+        return $this->houseService->createHouse($request);
     }
 
     public function show(House $house)
     {
-        return response()->json($house);
+        return $this->houseService->showHouse($house);
     }
 
     public function update(Request $request, House $house)
     {
-        $validatedData = $request->validate([
-            'house_title' => 'string',
-            'house_description' => 'string',
-            'rent_price' => 'integer',
-            'house_picture' => 'string',
-            'bedrooms' => 'integer',
-            'restrooms' => 'integer',
-            'bills_included' => 'boolean',
-            'street' => 'string',
-            'neighborhood' => 'string',
-            'city' => 'string',
-            'state' => 'string'
-        ]);
-
-        $house->update($validatedData);
-        return response()->json($house, 200);
+        return $this->houseService->updateHouse($request, $house);
     }
 
     public function delete(House $house)
     {
         Comment::where('house_id', $house->id)->delete();
-        $house->delete();
-        return response()->json(null, 204);
+        return $this->houseService->deleteHouse($house);
     }
 
 }
---
+++ backend/app/Services/HouseService.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\House;
+
+class HouseService
+{
+    public function findAllHouses()
+    {
+        $houses = House::all();
+        return response()->json($houses);
+    }
+
+    public function createHouse($request)
+    {
+        $validatedData = $request->validate([
+            'user_id' => 'required|integer|exists:users,id',
+            'house_title' => 'required|string',
+            'house_description' => 'required|string',
+            'rent_price' => 'required|integer',
+            'house_picture' => 'required|string',
+            'bedrooms' => 'required|integer',
+            'restrooms' => 'required|integer',
+            'bills_included' => 'required|boolean',
+            'street' => 'required|string',
+            'neighborhood' => 'required|string',
+            'city' => 'required|string',
+            'state' => 'required|string'
+        ]);
+
+        $house = House::create($validatedData);
+        return response()->json($house, 201);
+    }
+
+    public function showHouse($house)
+    {
+        return response()->json($house);
+    }
+
+    public function updateHouse($request, $house)
+    {
+        $validatedData = $request->validate([
+            'house_title' => 'string',
+            'house_description' => 'string',
+            'rent_price' => 'integer',
+            'house_picture' => 'string',
+            'bedrooms' => 'integer',
+            'restrooms' => 'integer',
+            'bills_included' => 'boolean',
+            'street' => 'string',
+            'neighborhood' => 'string',
+            'city' => 'string',
+            'state' => 'string'
+        ]);
+
+        $house->update($validatedData);
+        return response()->json($house, 200);
+    }
+
+    public function deleteHouse($house)
+    {
+        $house->delete();
+        return response()->json(null, 204);
+    }
+}
