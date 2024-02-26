cc5ef33399844ecfc8acb867ea0ba323c1f1438c
belloLucas <lucasbellodev@gmail.com>
2024-02-26 15:33:31 -0300

    feat: Extracting Login and Register form style to a separate file

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,15 +1,40 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const form = ref({
+  email: "",
+  password: "",
+});
+
+const handleLogin = async () => {
+  await axios.post("/login", {
+    email: form.value.email,
+    password: form.value.password,
+  });
+  router.push("/");
+};
+</script>
 
 <template>
   <section>
     <div class="container">
       <div class="wrapper">
         <h4>Acesse sua conta</h4>
-        <form>
+        <form @submit.prevent="handleLogin">
           <div class="inputs">
-            <input type="email" name="email" id="email" placeholder="Email" />
+            <input
+              type="email"
+              v-model="form.email"
+              name="email"
+              id="email"
+              placeholder="Email"
+            />
             <input
               type="password"
+              v-model="form.password"
               name="password"
               id="password"
               placeholder="Senha"
@@ -28,66 +53,5 @@
 
 <style lang="scss">
 @import "../assets/scss/app.scss";
-
-.container {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 30%;
-  height: 70%;
-  margin-top: 2rem;
-
-  background-color: $white;
-  border: 1px solid $box-title-grey;
-  border-radius: 2px;
-
-  .wrapper {
-    width: 90%;
-    height: 80%;
-  }
-
-  h4 {
-    margin: 10px 0 35px 0;
-    text-align: center;
-  }
-
-  .inputs {
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    margin-bottom: 20px;
-
-    input {
-      padding: 7px;
-      border: 1px solid $grey;
-      border-radius: 5px;
-      outline: none;
-    }
-  }
-  .buttons {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    flex-direction: column;
-    gap: 20px;
-
-    a {
-      font-size: 14px;
-    }
-
-    button {
-      width: 100%;
-      padding: 8px;
-      cursor: pointer;
-      background-color: $grey-blue;
-      color: white;
-      border: none;
-    }
-  }
-}
+@import "../assets/scss/form.scss";
 </style>
--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -7,12 +7,19 @@
         <h4>Crie sua conta</h4>
         <form>
           <div class="inputs">
+            <input type="text" name="name" id="name" placeholder="Nome" />
             <input type="email" name="email" id="email" placeholder="Email" />
             <input
               type="password"
               name="password"
               id="password"
               placeholder="Senha"
+            />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Confirme sua senha"
             />
           </div>
           <div class="buttons">
@@ -27,4 +34,5 @@
 
 <style lang="scss">
 @import "../assets/scss/app.scss";
+@import "../assets/scss/form.scss";
 </style>
