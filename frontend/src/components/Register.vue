20c67b5cf55aa29457578a17db09affa871fc1a6
belloLucas <lucasbellodev@gmail.com>
2024-02-26 16:00:48 -0300

    feat: Register now implemented

--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -1,25 +1,61 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const form = ref({
+  name: "",
+  email: "",
+  password: "",
+  password_confirmation: "",
+});
+
+const handleRegister = async () => {
+  await axios.post("/register", {
+    name: form.value.name,
+    email: form.value.email,
+    password: form.value.password,
+    password_confirmation: form.value.password_confirmation,
+  });
+  router.push("/");
+};
+</script>
 
 <template>
   <section>
     <div class="container">
       <div class="wrapper">
         <h4>Crie sua conta</h4>
-        <form>
+        <form @submit.prevent="handleRegister">
           <div class="inputs">
-            <input type="text" name="name" id="name" placeholder="Nome" />
-            <input type="email" name="email" id="email" placeholder="Email" />
             <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="Senha"
+              type="text"
+              placeholder="Nome"
+              v-model="form.name"
+              name="name"
+              id="name"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              v-model="form.email"
+              name="email"
+              id="email"
             />
             <input
               type="password"
+              placeholder="Senha"
+              v-model="form.password"
               name="password"
               id="password"
+            />
+            <input
+              type="password"
               placeholder="Confirme sua senha"
+              v-model="form.password_confirmation"
+              name="password"
+              id="password_confirmation"
             />
           </div>
           <div class="buttons">
