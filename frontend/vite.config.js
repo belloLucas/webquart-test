400d1a9ce2bd372835fbbb146b38226b06466399
belloLucas <lucasbellodev@gmail.com>
2024-02-28 19:00:08 -0300

    feat: Fixing application port to :5173

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  preview: {
+    port: 5173,
+  },
+  server: {
+    port: 5173,
+  },
 });
